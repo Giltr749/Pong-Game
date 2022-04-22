7bf9f2288c081d4ae52cfe0d81fdd0e815c6fffd
jonathanwitt <joniwitt79@gmail.com>
2022-04-22 11:02:25 +0300

    basic layout of the gameboard but i dont know if its useable

--- fe/pong/src/App.js
+++ fe/pong/src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Ball from './Components/Ball.jsx'
+import "./App.css";
+import Ball from "./Components/Ball.jsx";
 
 function App() {
-
-  const ROW_SIZE = 10; // should be sent to ball component as props
-  const COL_SIZE = 20; // should be sent to ball component as props
+  const ROW_SIZE = 20; // should be sent to ball component as props
+  const COL_SIZE = 40; // should be sent to ball component as props
   const board = [...Array(ROW_SIZE * COL_SIZE)];
+  console.log(board);
 
   return (
     <div className="App">
-      <Ball />
+      <div className="board">
+        {board.map((box, index) => (
+          <li key={index}>{box}</li>
+        ))}
+        <Ball />
+      </div>
     </div>
   );
 }

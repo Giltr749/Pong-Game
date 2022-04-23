f2db77b9b8a2873a6c11d7a518f7fbc75955c1b1
jonathanwitt <joniwitt79@gmail.com>
2022-04-23 13:44:14 +0300

    css and basic setup for ball movement

--- fe/pong/src/App.js
+++ fe/pong/src/App.js
@@ -1,22 +1,10 @@
-import "./App.css";
-import Ball from "./Components/Ball.jsx";
+import React from "react";
+import PingPongPage from "./pages/PingPongPage";
 
-function App() {
-  const ROW_SIZE = 20; // should be sent to ball component as props
-  const COL_SIZE = 40; // should be sent to ball component as props
-  const board = [...Array(ROW_SIZE * COL_SIZE)];
-  console.log(board);
-
+export default function App() {
   return (
-    <div className="App">
-      <div className="board">
-        {board.map((box, index) => (
-          <li key={index}>{box}</li>
-        ))}
-        <Ball />
-      </div>
+    <div>
+      <PingPongPage />
     </div>
   );
 }
-
-export default App;
--- fe/pong/src/Components/OneBox.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./OneBox.css";
-
-export default function OneBox() {
-  return <div className="one-box"></div>;
-}

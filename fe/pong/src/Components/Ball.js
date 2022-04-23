c403e8ab37d646e6444fcd40a1c79cd87a4010dd
jonathanwitt <joniwitt79@gmail.com>
2022-04-23 17:29:14 +0300

    ball bouncing of all directions

--- fe/pong/src/Components/Ball.js
+++ fe/pong/src/Components/Ball.js
@@ -36,13 +36,18 @@ export default function Ball({
     document.documentElement.style.setProperty("--y", positionY);
   }
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setX(setPositionX((positionX += deltaX)));
-  //     setY(setPositionY((positionY += deltaY)));
-  //   }, 100);
-  //   return () => clearInterval(interval);
-  // }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setX(setPositionX((positionX += deltaX)));
+      setY(setPositionY((positionY += deltaY)));
+      if (positionY > 99 || positionY < 1) {
+        deltaY *= -1;
+      } else if (positionX < 1 || positionX > 99) {
+        deltaX *= -1;
+      }
+    }, 10);
+    return () => clearInterval(interval);
+  }, []);
 
   // function reset() {
   //   while (true) {

e862d006b23d3fa9dda10c1bc7fdcc43fb69dcf6
jonathanwitt <joniwitt79@gmail.com>
2022-04-24 21:17:34 +0300

    added just go to game button but we have some error in the dev tools

--- be/src/server.js
+++ be/src/server.js
@@ -49,7 +49,7 @@ app.post("/login", jsonParser, async (req, res) => {
   const getUser = `SELECT * FROM users WHERE nickname ='${req.body.nickname}';`;
 
   con.query(getUser, async (err, result) => {
-    if (err) send(err);
+    if (err) res.send("hello");
 
     bcrypt.compare(req.body.password, result[0].password, (err, checkRes) => {
       if (checkRes) {
@@ -59,6 +59,51 @@ app.post("/login", jsonParser, async (req, res) => {
       }
     });
   });
+});
+
+app.post("/login", function (request, response) {
+  // Capture the input fields
+  let nickname = request.body.nickname;
+  let password = request.body.password;
+  // Ensure the input fields exists and are not empty
+  if (nickname && password) {
+    // Execute SQL query that'll select the account from the database based on the specified username and password
+    con.query(
+      "SELECT * FROM users WHERE nickname = ? AND password = ?",
+      [username, password],
+      function (error, results, fields) {
+        // If there is an issue with the query, output the error
+        if (error) throw error;
+        // If the account exists
+        if (results.length > 0) {
+          // Authenticate the user
+          request.session.loggedin = true;
+          request.session.username = username;
+          // Redirect to home page
+          response.redirect("/home");
+        } else {
+          response.send("Incorrect Username and/or Password!");
+        }
+        response.end();
+      }
+    );
+  } else {
+    response.send("Please enter Username and Password!");
+    response.end();
+  }
+});
+
+// http://localhost:3000/home
+app.get("/home", function (request, response) {
+  // If the user is loggedin
+  if (request.session.loggedin) {
+    // Output username
+    response.send("Welcome back, " + request.session.username + "!");
+  } else {
+    // Not logged in
+    response.send("Please login to view this page!");
+  }
+  response.end();
 });
 
 app.listen(PORT, () => {
--- fe/pong/src/Components/Login.js
+++ fe/pong/src/Components/Login.js
@@ -13,20 +13,46 @@ function Login(props) {
     navigate("/signup");
   }
 
+  function toGame() {
+    navigate("/pong");
+  }
+
+  async function login() {
+    const user = { nickname: nick, password: pass };
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(await response);
+    return await response;
+  }
+
   return (
-    <div className="login-div">
-      <h2>Login</h2>
-      <label>Nickname:</label>
-      <input type="text" onChange={(e) => setNick(e.target.value)}></input>
-      <label>Password:</label>
-      <input type="password" onChange={(e) => setPass(e.target.value)}></input>
-      <p>
-        Don't have an account?{" "}
-        <span onClick={toSignup} style={{ textDecoration: "underline" }}>
-          Sign up
-        </span>
-      </p>
-    </div>
+    <>
+      <div className="login-div">
+        <h2>Login</h2>
+        <label>Nickname:</label>
+        <input type="text" onChange={(e) => setNick(e.target.value)}></input>
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPass(e.target.value)}
+        ></input>
+        <button onClick={login}>Login</button>
+        <p>
+          Don't have an account?{" "}
+          <span onClick={toSignup} style={{ textDecoration: "underline" }}>
+            Sign up
+          </span>
+        </p>
+      </div>
+      <button className="login-div" id="goGame" onClick={toGame}>
+        Go to Game
+      </button>
+    </>
   );
 }
 

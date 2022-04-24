ea07eb72fb6d3ca908d0a3f26b45819e5f3a25bd
Giltr749 <97340782+Giltr749@users.noreply.github.com>
2022-04-24 19:17:01 +0300

    game end

--- fe/pong/src/App.js
+++ fe/pong/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="/pong" element={<PingPongPage />} />
       </Routes> */}
-      <PingPongPage/>
+      
     </div>
   );
 }
